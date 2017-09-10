8f4347c55a253f47e4f3b82062c984aacda0ce11
rajamuhammad <rajamuhammad53@gmail.com>
2017-09-10 09:55:06 -0700

    Application

---
+++ WebApplication2/Controllers/RecipeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class RecipeController : Controller
+    {
+        List<Recipe> arr = new List<Recipe>();
+        public RecipeController()
+        {
+
+            arr.Add(new Recipe() { Id = 1, Name = "Abc", Ingredients = "asjdhasjkhdjkh", Method = "sjkdklasjklhdjkhasjk" });
+            arr.Add(new Recipe() { Id = 1, Name = "Abc", Ingredients = "asjdhasjkhdjkh", Method = "sjkdklasjklhdjkhasjk" });
+            arr.Add(new Recipe() { Id = 1, Name = "Abc", Ingredients = "asjdhasjkhdjkh", Method = "sjkdklasjklhdjkhasjk" });
+           
+        }
+
+
+        public ActionResult Index()
+        {
+            return View(arr);
+        }
+
+       
+        public ActionResult create()
+        {
+            return View();
+        }
+
+         [HttpPost]
+
+        public ActionResult create(Recipe rec)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var ed = arr.Find(d=> d.Id == id);
+            return View(ed);
+
+        }
+        public ActionResult Details(int id)
+        {
+
+            var ed = arr.Find(d => d.Id == id);
+            return View(ed);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            var ed = arr.Find(d => d.Id == id);
+            return View(ed);
+
+        }
+	}
+}
---
+++ WebApplication2/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Ingredients { get; set; }
+        public String Method { get; set; }
+    }
+}
